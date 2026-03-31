14b3c69ff2dd04dff43cfe1844965d94b5f1ea10
Arjun-Parajuli <chhetriarjun55555@gmail.com>
2026-03-31 09:26:21 +0530

    Terraform vpc private subnet

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -7,5 +7,15 @@ resource "aws_vpc" "main" {
     Name                                           = "${var.cluster_name}-vpc"
     "kubernetes.io/cluster/${var.cluster_name}"    = "shared"
   }
+}
 
+resource "aws_subnet" "private" {
+  count = length(var.private_subnet_cidrs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    "Name" = "${var.cluster_name}-private-${count.index + 1}"
+  }
 }
--- terraform/vpc/variables.tf
+++ terraform/vpc/variables.tf
@@ -7,3 +7,13 @@ variable "cluster_name" {
   description = "Name of the EKS cluster"
   type        = string
 }
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "Availability zones"
+  type        = list(string)
+}
