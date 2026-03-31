f1e576c605c20f51505776f37060aac3d2023a74
Arjun-Parajuli <chhetriarjun55555@gmail.com>
2026-03-31 20:21:23 +0530

    Terraform vpc added public and private route tables

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -63,3 +63,30 @@ resource "aws_nat_gateway" "main" {
     Name = "${var.cluster_name}-nat-${count.index + 1}"
   }
 }
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "${var.cluster_name}-public"
+  }
+}
+
+resource "aws_route_table" "private" {
+  count  = length(var.private_subnet_cidrs)
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main[count.index].id
+  }
+
+  tags = {
+    Name = "${var.cluster_name}-private-${count.index + 1}"
+  }
+}
