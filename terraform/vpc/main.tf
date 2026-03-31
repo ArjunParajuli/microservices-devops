bb9f027b0cb12a5c2a20f77584a4f4ab87e636cb
Arjun-Parajuli <chhetriarjun55555@gmail.com>
2026-03-31 20:22:12 +0530

    Terraform vpc: route table association with subnet

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -90,3 +90,15 @@ resource "aws_route_table" "private" {
     Name = "${var.cluster_name}-private-${count.index + 1}"
   }
 }
+
+resource "aws_route_table_association" "private" {
+  count          = length(var.private_subnet_cidrs)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnet_cidrs)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
